c1efcae357ad4f06ad00da0ecb9b4bbe45a1254e
2472545 <2472545@edu.vaniercollege.qc.ca>
2025-09-02 16:53:36 -0400

    errorlab1.py

--- lab1.py
+++ lab1.py
@@ -1,6 +1,1 @@
-print("Hello, world!") #Question 1
-name = input("Please enter your name. ")
-name = str(name)
-age = input("Please enter your age. ")
-age = int(age)
-print("Hello " + name + "!")
+
